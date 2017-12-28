5dfff803c973f64ae63bbf811b40810544e0029c
liumapp <liumapp.com@gmail.com>
2017-12-28 16:01:56 +0800

    add simple job

---
+++ src/main/java/com/liumapp/schedule/demo/jobs/SimpleJob.java
@@ -0,0 +1,23 @@
+package com.liumapp.schedule.demo.jobs;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+/**
+ * Created by liumapp on 12/28/17.
+ * E-mail:liumapp.com@gmail.com
+ * home-page:http://www.liumapp.com
+ */
+public class SimpleJob implements Job {
+
+    public SimpleJob() {
+
+    }
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        System.out.println("Hello world , a simple job is running ! ");
+    }
+
+}
