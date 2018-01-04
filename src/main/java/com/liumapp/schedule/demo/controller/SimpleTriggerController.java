a4a5530d5065c41bef8b174ecbbf22a80d3a4803
liumapp <liumapp.com@gmail.com>
2018-01-04 10:49:39 +0800

    simple trigger modified

--- src/main/java/com/liumapp/schedule/demo/controller/SimpleTriggerController.java
+++ src/main/java/com/liumapp/schedule/demo/controller/SimpleTriggerController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by liumapp on 1/2/18.
