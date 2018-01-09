79dfe69002ae977b993cbe7add1b2b6f05f9a3e4
liumapp <liumapp.com@gmail.com>
2018-01-09 15:11:47 +0800

    use springrunner

--- src/test/java/com/liumapp/schedule/demo/jobs/HelloTest.java
+++ src/test/java/com/liumapp/schedule/demo/jobs/HelloTest.java
@@ -1,13 +1,28 @@
 package com.liumapp.schedule.demo.jobs;
 
+import com.liumapp.schedule.demo.Main;
 import org.junit.runner.RunWith;
+import org.quartz.Scheduler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * Created by liumapp on 1/9/18.
  * E-mail:liumapp.com@gmail.com
  * home-page:http://www.liumapp.com
  */
-@RunWith(SpringBootApplication.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {Main.class})
 public class HelloTest {
+
+    @Autowired
+    private Scheduler scheduler;
+
+    public void testHello () {
+
+    }
+
+
 }
