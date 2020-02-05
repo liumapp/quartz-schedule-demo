91d198c9a36aa85184bb05dab04205d91e4c0790
liumapp <liumapp.com@gmail.com>
2020-02-05 17:14:20 +0800

    update quartz job sql file

--- quartz_job.sql
+++ quartz_job.sql
@@ -7,17 +7,17 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `quartz_job`;
 CREATE TABLE `quartz_job` (
   `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `job_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '任务名称',
-  `job_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT 'jobid',
-  `group_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT 'job组',
-  `trigger_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '调度id',
-  `params_json` varchar(21845) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '参数',
-  `status` tinyint(2) DEFAULT 0 COMMENT '状态 0.待执行 1.执行完成 2.执行失败',
-  `job_class` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT 'job类',
-  `job_params_class` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT 'job参数类',
-  `exec_time` datetime(0) DEFAULT NULL COMMENT '执行时间',
-  `create_time` datetime(0) DEFAULT NULL,
-  `is_delete` tinyint(2) DEFAULT 0,
+  `job_name` varchar(255) NULL COMMENT '任务名称',
+  `job_id` varchar(255) NULL COMMENT 'jobid',
+  `group_id` varchar(255) NULL COMMENT 'job组',
+  `trigger_id` varchar(255) NULL COMMENT '调度id',
+  `params_json` varchar(11845) NULL COMMENT '参数',
+  `status` tinyint(1) UNSIGNED DEFAULT 0 COMMENT '状态 0.待执行 1.执行完成 2.执行失败',
+  `job_class` varchar(255) NULL COMMENT 'job类',
+  `job_params_class` varchar(255) NULL COMMENT 'job参数类',
+  `exec_time` datetime NULL,
+  `create_time` datetime NULL,
+  `is_delete` tinyint(1) UNSIGNED DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
