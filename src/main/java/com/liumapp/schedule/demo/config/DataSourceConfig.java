c0b6568bae1d99411ec8bfe8ed3783af82a9ceaf
liumapp <liumapp.com@gmail.com>
2020-02-05 14:27:14 +0800

    update quartz job manager and its data source config class

--- src/main/java/com/liumapp/schedule/demo/config/DataSourceConfig.java
+++ src/main/java/com/liumapp/schedule/demo/config/DataSourceConfig.java
@@ -1,7 +1,17 @@
 package com.liumapp.schedule.demo.config;
 
+import org.apache.ibatis.mapping.Environment;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.ibatis.transaction.TransactionFactory;
+import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import tk.mybatis.spring.annotation.MapperScan;
+
+import javax.sql.DataSource;
 
 /**
  * file DataSourceConfig.java
@@ -13,8 +23,20 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @ConditionalOnProperty(name = "com.liumapp.schedule.datasource")
+@MapperScan(value = "com.liumapp.schedule.demo.mapper", sqlSessionTemplateRef = "authManagerSqlSessionTemplate" )
 public class DataSourceConfig {
 
-
+    @Bean("authManagerSqlSessionTemplate")
+    public SqlSessionTemplate commmonSessionTemplate() {
+        DataSource dataSource = this.deviceDataSource();
+        TransactionFactory transactionFactory = new JdbcTransactionFactory();
+        Environment environment = new Environment("device", transactionFactory, dataSource);
+        tk.mybatis.mapper.session.Configuration configuration = new tk.mybatis.mapper.session.Configuration();
+        configuration.setEnvironment(environment);
+        configuration.setLazyLoadingEnabled(true);
+        configuration.setMapUnderscoreToCamelCase(true);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
 
 }
--- src/main/java/com/liumapp/schedule/demo/mapper/QuartzJobMapper.java
+++ src/main/java/com/liumapp/schedule/demo/mapper/QuartzJobMapper.java
@@ -1,32 +1,11 @@
 package com.liumapp.schedule.demo.mapper;
 
 import com.liumapp.schedule.demo.domain.QuartzJob;
-import com.liumapp.schedule.demo.domain.QuartzJobExample;
-import java.util.List;
-import org.apache.ibatis.annotations.Param;
-import org.springframework.stereotype.Repository;
+import tk.mybatis.mapper.common.Mapper;
 
-@Repository
-public interface QuartzJobMapper {
-    long countByExample(QuartzJobExample example);
+@org.apache.ibatis.annotations.Mapper
+public interface QuartzJobMapper extends Mapper<QuartzJob>{
 
-    int deleteByExample(QuartzJobExample example);
 
-    int deleteByPrimaryKey(String id);
 
-    int insert(QuartzJob record);
-
-    int insertSelective(QuartzJob record);
-
-    List<QuartzJob> selectByExample(QuartzJobExample example);
-
-    QuartzJob selectByPrimaryKey(String id);
-
-    int updateByExampleSelective(@Param("record") QuartzJob record, @Param("example") QuartzJobExample example);
-
-    int updateByExample(@Param("record") QuartzJob record, @Param("example") QuartzJobExample example);
-
-    int updateByPrimaryKeySelective(QuartzJob record);
-
-    int updateByPrimaryKey(QuartzJob record);
 }
