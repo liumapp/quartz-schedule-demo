be9c1839bf72df4a1280635bea631e694bc2b0b5
liumapp <liumapp.com@gmail.com>
2020-02-07 10:09:09 +0800

    create make apple job test class

---
+++ src/test/java/com/liumapp/schedule/demo/jobs/MakeAppleJobTest.java
@@ -0,0 +1,29 @@
+package com.liumapp.schedule.demo.jobs;
+
+import com.liumapp.schedule.demo.Main;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * file MakeAppleJobTest.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2020/2/7
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {Main.class})
+public class MakeAppleJobTest {
+
+    @Test
+    public void execute() {
+
+
+
+    }
+}
