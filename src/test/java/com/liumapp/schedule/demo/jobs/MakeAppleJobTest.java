e879e8e92b4954ebfcabefcd3affcc03ff3037d2
liumapp <liumapp.com@gmail.com>
2020-02-07 11:22:45 +0800

    update make apple job test

--- src/test/java/com/liumapp/schedule/demo/jobs/MakeAppleJobTest.java
+++ src/test/java/com/liumapp/schedule/demo/jobs/MakeAppleJobTest.java
@@ -2,12 +2,16 @@ package com.liumapp.schedule.demo.jobs;
 
 import com.liumapp.schedule.demo.Main;
 import com.liumapp.schedule.demo.dto.AppleParams;
+import com.liumapp.schedule.demo.mapper.QuartzJobMapper;
 import com.liumapp.schedule.demo.model.QuartzJob;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.StopWatch;
 
+import java.util.Date;
 import java.util.UUID;
 
 import static org.junit.Assert.*;
@@ -24,21 +28,38 @@ import static org.junit.Assert.*;
 @SpringBootTest(classes = {Main.class})
 public class MakeAppleJobTest {
 
+    @Autowired
+    private QuartzJobMapper quartzJobMapper;
+
+    /**
+     * save quartz job info to db
+     */
     @Test
     public void createMissionToDb () {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
         AppleParams appleParams = new AppleParams(UUID.randomUUID().getLeastSignificantBits(), "red", 3.5F);
         QuartzJob quartzJob = QuartzJob.builder()
                 .id(appleParams.getId())
                 .jobName("make-apple")
                 .groupId("make-apple-group-01")
                 .triggerId("make-apple")
-
+                .paramsJson(appleParams.toJsonParams())
+                .jobClass(MakeAppleJob.class.getName())
+                .jobParamsClass(AppleParams.class.getName())
+                .createTime(new Date())
                 .build();
+        quartzJobMapper.insertSelective(quartzJob);
+        stopWatch.stop();
+        System.out.println(stopWatch.getTotalTimeMillis() + "ms");
     }
 
+    /**
+     * load mission from db and execute it .
+     */
     @Test
     public void execute() {
-
+        
     }
 
 
