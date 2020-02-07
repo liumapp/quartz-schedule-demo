5ca00062ae8c2bf212a0d11db64ff681c9615cfc
liumapp <liumapp.com@gmail.com>
2020-02-07 20:36:41 +0800

    update make apple job

--- src/main/java/com/liumapp/schedule/demo/jobs/MakeAppleJob.java
+++ src/main/java/com/liumapp/schedule/demo/jobs/MakeAppleJob.java
@@ -1,8 +1,12 @@
 package com.liumapp.schedule.demo.jobs;
 
+import com.alibaba.fastjson.JSON;
+import com.liumapp.schedule.demo.dto.AppleParams;
+import com.liumapp.schedule.demo.dto.BaseJobParams;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.quartz.impl.JobExecutionContextImpl;
 import org.springframework.util.StopWatch;
 
 /**
@@ -20,7 +24,10 @@ public class MakeAppleJob implements Job {
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
         System.out.println("ready to make apple");
-
+        AppleParams appleParams = JSON.parseObject(
+                (String) jobExecutionContext.getMergedJobDataMap().get(BaseJobParams.jobDataKey),
+                AppleParams.class
+        );
         stopWatch.stop();
         System.out.println(stopWatch.prettyPrint());
     }
